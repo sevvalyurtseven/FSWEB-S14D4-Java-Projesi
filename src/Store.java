6c0469954aef315315a2d22dbb6efbbbf46d8e2e
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-22 17:59:01 +0300

    listProducts methodu guncellendi

--- src/Store.java
+++ src/Store.java
@@ -1,3 +1,5 @@
+import com.workintech.store.model.ProductForSale;
+
 public class Store {
     public static void main(String[] args) {
 
@@ -5,6 +7,11 @@ public class Store {
     }
 
     public static void listProducts(ProductForSale[] products) {
+        for (ProductForSale product : products){
+            if(product!=null){
+                product.showDetails();
+            }
+        }
 
     }
 }
