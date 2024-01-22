04c188920021f1dc8131644882e20ba845ae470c
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-22 18:04:17 +0300

    objeler olusturuldu

--- src/Store.java
+++ src/Store.java
@@ -1,7 +1,15 @@
+import com.workintech.store.model.Bread;
+import com.workintech.store.model.Chocolate;
+import com.workintech.store.model.Coke;
 import com.workintech.store.model.ProductForSale;
 
 public class Store {
     public static void main(String[] args) {
+        ProductForSale[] product = new ProductForSale[5];
+        product[0] = new Chocolate("Sweet", 13.4, "Chocolate Object", "White", Boolean.TRUE);
+        product[1] = new Bread("Bakery", 5, "Bread Object", "Einkorn", "Brown");
+        product[2] = new Coke("Drinks", 15, "Coke Object", Boolean.TRUE, 2.5);
+
 
         listProducts();
     }
