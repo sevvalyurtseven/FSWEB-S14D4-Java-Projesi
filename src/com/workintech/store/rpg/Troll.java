e69cc35d5eb011eebfb1bd8ee41866747a480a83
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-22 18:18:50 +0300

    extend ve implenet edilen siniflarin gereklilikleri olusturuldu

--- src/com/workintech/store/rpg/Troll.java
+++ src/com/workintech/store/rpg/Troll.java
@@ -1,4 +1,17 @@
 package com.workintech.store.rpg;
 
 public class Troll extends Monster implements Bleedable, Poisonable{
+    public Troll(String name, int hitPoints, double damage) {
+        super(name, hitPoints, damage);
+    }
+
+    @Override
+    public double bleed() {
+        return 0;
+    }
+
+    @Override
+    public double poison() {
+        return 0;
+    }
 }
