7d85c738368414347368070a180609497f723585
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-22 17:48:35 +0300

    bread sinifina özgü instance variable'lar olusturuldu constructor guncellendi

--- src/com/workintech/store/model/Bread.java
+++ src/com/workintech/store/model/Bread.java
@@ -1,8 +1,12 @@
 package com.workintech.store.model;
 
 public class Bread extends ProductForSale{
-    public Bread(String type, double price, String description) {
+    private String floorType;
+    private String color;
+    public Bread(String type, double price, String description, String floorType, String color ) {
         super(type, price, description);
+        this.floorType = floorType;
+        this.color = color;
     }
 
     @Override
