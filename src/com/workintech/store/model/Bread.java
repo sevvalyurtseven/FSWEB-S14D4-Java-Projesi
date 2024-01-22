af3808ad65c6e2e8bfc8430a49ee903cf959fd45
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-22 17:45:39 +0300

    Bread sinifi abstract olan ProductForSale sinifina extend edildi ve parent sinifin zorunluluklari olusturuldu

---
+++ src/com/workintech/store/model/Bread.java
@@ -0,0 +1,12 @@
+package com.workintech.store.model;
+
+public class Bread extends ProductForSale{
+    public Bread(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+    }
+}
