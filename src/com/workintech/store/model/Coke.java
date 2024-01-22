dfa585e4d44de120ea9e6b926ff361b37feb0aae
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-22 17:52:23 +0300

    parent sinifin constructor ve abstract methodu child sinifa aktarildi

---
+++ src/com/workintech/store/model/Coke.java
@@ -0,0 +1,12 @@
+package com.workintech.store.model;
+
+public class Coke extends ProductForSale{
+    public Coke(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+
+    }
+}
