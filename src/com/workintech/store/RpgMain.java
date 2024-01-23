1b5d938f92f9df9e291698155c59eb0310ed407a
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-23 19:31:46 +0300

    Monster For Simple RPG Game uygulamasi test edildi

--- src/com/workintech/store/RpgMain.java
+++ src/com/workintech/store/RpgMain.java
@@ -3,12 +3,16 @@ package com.workintech.store;
 
 import com.workintech.store.rpg.Monster;
 import com.workintech.store.rpg.Troll;
+import com.workintech.store.rpg.Werewolf;
 
 public class RpgMain {
     public static void main(String[] args) {
         Monster shrek = new Troll("Shrek", 1000, 40);
+        Monster wolf = new Werewolf("Wolf", 500, 100);
+        printAttackResult(shrek);
+        printAttackResult(wolf);
     }
-    public void printAttackResult(Monster monster){
-        System.out.println("Attack Result: " + monster.attack());
+    private static void printAttackResult(Monster monster){
+        System.out.println("Attack Result of "+ monster.getName() + " : " + monster.attack());
     }
 }
